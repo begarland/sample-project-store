e855526315a83b1791957092fd2b2b873714152f
begarland <brandy.e.garland@gmail.com>
2024-02-24 18:16:04 -0700

    get swagger working

--- src/items/dto/create-item.dto.ts
+++ src/items/dto/create-item.dto.ts
@@ -1,1 +1,10 @@
-export class CreateItemDto {}
+export class CreateItemDto {
+  /**
+   * The name of the item
+   * @example 'cat food'
+   */
+  name: string;
+  //   price: number;
+
+  //   description?: string;
+}
